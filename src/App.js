4f2333feee725071024506f81de13a9f667522dc
deja.craft <dejacraft28@gmail.com>
2023-03-20 12:43:40 -0400

    SportsProject

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from "react-router-dom";
+import {useState, useEffect} from "react";
 import List from './components/List';
 import Nav from './components/Nav';
 import Home from './components/Home';
 import Stats from './components/Stats';
 
-
 function App(props) {
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '440d7f76bbmsh4facf782795d9d5p1b51a0jsn6de6932883eb',
+      'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://api-nba-v1.p.rapidapi.com/teams/statistics?id=1&season=2020', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+
+
   return (
     <div className="App">
 <Nav/>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,14 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className='Home'> </div>
+    <div className='Home'>
+         <h1>NBA Stats App</h1> 
+
+<img className='image1' src="https://repository-images.githubusercontent.com/200110538/d145dc80-775a-11ea-829a-390d464f83b5" alt="React Image" />
+
+
+        
+         </div>
   )
 }
 
