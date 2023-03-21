5a0bc279e2e483692188e4e0661f2f1b25aa82ea
deja.craft <dejacraft28@gmail.com>
2023-03-21 13:16:27 -0400

    SportsProject

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,34 +1,39 @@
-import React from 'react'
-import Cards from '../components/Cards';
-import {useState, useEffect} from "react";
-
+import React from "react";
+import Cards from "../components/Cards";
+import { useState, useEffect } from "react";
 
 function List({ handleOnClick }) {
-  const apiKey = "440d7f76bbmsh4facf782795d9d5p1b51a0jsn6de6932883eb";
-  const [team, setTeam] = useState([])
-
-    useEffect(() => {
-        //Function to Fetch Data
-        const getData = async () => {
-            const response = await fetch('https://api-nba-v1.p.rapidapi.com/teams/statistics?id=1&season=2020')
-            const data = await response.json()
-            const teamList = data.results.slice(0, data.results.length).map(item => item).flat()
-            setTeam(teamList)
-        }
-    
-        getData()
-    }, [])
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '440d7f76bbmsh4facf782795d9d5p1b51a0jsn6de6932883eb',
+      'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+    }
+  };
   
+
+  const [teams, setTeam] = useState([]);
+
+  useEffect(() => {
+    //Function to Fetch Data
+    const getData = async () => {
+    fetch('https://api-nba-v1.p.rapidapi.com/teams?page=0', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+    };
+
+    getData();
+  }, []);
+
   return (
-    <div className='List'> 
+    <div className="List">
+      {teams.map((TeamClass, id) => {
+        return <Cards key={id} TeamClass={id} handleOnClick={handleOnClick} />;
+      })}
     
-    {team.map((TeamClass, id) => {
-                return (
-                    <Cards key={id} TeamClass={id} handleOnClick={handleOnClick} />
-                )
-            })}
     </div>
-  )
+  );
 }
 
-export default List
+export default List;
