e474ec6b9a9b1add70e8d6d424cc5963cc4e7391
deja.craft <dejacraft28@gmail.com>
2023-03-23 18:11:57 -0400

    NBA-App

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
     <div className='Home'>
          <h1 className='Header'>View all NBA Teams in one Place!</h1> 
 
-<img className='image1' src="https://images.squarespace-cdn.com/content/v1/576ee55c15d5dbd84d9ac8af/1563261145661-WOPDXMLZQOCK3G8YCSA8/player+circle.gif
+<img className='image1' src="https://images.squarespace-cdn.com/content/v1/52850829e4b0b6141ccc4fb5/1575867458905-3RRKWENE2YGJ52BBIL6Z/FullSetTeamWithIP-01.gif
 
 " alt="" />
 
