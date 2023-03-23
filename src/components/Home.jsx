8e895aa6fd8cc0cc69787d38427c3a4401422502
deja.craft <dejacraft28@gmail.com>
2023-03-23 17:49:59 -0400

    NBA-App

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div className='Home'>
-         <h1>View all NBA Teams in one Place!</h1> 
+         <h1 className='Header'>View all NBA Teams in one Place!</h1> 
 
-<img className='image1' src="https://images.squarespace-cdn.com/content/v1/576ee55c15d5dbd84d9ac8af/1563261145661-WOPDXMLZQOCK3G8YCSA8/player+circle.gif" alt="React Image" />
+<img className='image1' src="https://images.squarespace-cdn.com/content/v1/576ee55c15d5dbd84d9ac8af/1563261145661-WOPDXMLZQOCK3G8YCSA8/player+circle.gif
+
+" alt="" />
 
 
         
